09dd914cf3a2d32049af92cbb7e4790a9521ba18
Matt Williams <Matthew.Williams@Softwire.com>
2024-11-19 15:04:27 +0000

    wip - hough circle detection

--- camera/shuffleboard/shuffleView.py
+++ camera/shuffleboard/shuffleView.py
@@ -4,12 +4,6 @@ from utils import get_limits
 
 img = cv2.imread('capture.png')
 
-blue = [147, 49, 33]
-redLower = np.array([160, 150, 20])
-redUpper = np.array([180, 255, 255])
-redLower2 = np.array([0, 150, 20])
-redUpper2 = np.array([20, 255, 255])
-
 surface_size = (382, 1622)
 
 corners = np.array([[0, 704], [0, 327], [1623, 627], [1575, 293]], dtype=np.float32)
@@ -18,36 +12,57 @@ newCorners = np.array([[0, 0], [surface_size[0], 0], [0, surface_size[1]], surfa
 T = cv2.getPerspectiveTransform(corners, newCorners)
 persImage = cv2.warpPerspective(img, T, surface_size)
 
-hsvImage = cv2.cvtColor(persImage, cv2.COLOR_BGR2HSV)
+gray = cv2.cvtColor(persImage, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (9, 9), 2)
+edges = cv2.Canny(blurred, 50, 150)
 
-blueLower, blueUpper = get_limits(blue)
+cv2.imwrite('gray.png', gray)
+cv2.imwrite('blurred.png', blurred)
 
-redMask1 = cv2.inRange(hsvImage, redLower, redUpper)
-redMask2 = cv2.inRange(hsvImage, redLower2, redUpper2)
-redMask = cv2.bitwise_or(redMask1, redMask2)
-redCounters = cv2.bitwise_and(persImage, persImage, mask=redMask)
+# https://docs.opencv.org/3.4/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
+circles = cv2.HoughCircles(
+    edges,
+    cv2.HOUGH_GRADIENT,
+    dp=1,
+    minDist=20,
+    param1=50,
+    param2=20,
+    minRadius=15,
+    maxRadius=35
+)
 
-countours, hierachy = cv2.findContours(redMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for x, y, radius in circles[0, :]:
+        center = (x, y)
+        print(radius)
+        cv2.circle(persImage, center, radius, (0, 255, 0), 2)
+        cv2.circle(persImage, center, 8, (0, 255, 0), -1)
+else:
+    raise Exception("No circles detected in the image")
 
-if len(countours) > 0:
-    for countour in countours:
-        if cv2.contourArea(countour) > 100:
-            x, y, w, h = cv2.boundingRect(countour)
-            cv2.rectangle(persImage, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# countours, hierachy = cv2.findContours(redMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-invRedMask = cv2.bitwise_not(redMask)
-countours, hierachy = cv2.findContours(invRedMask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-if len(countours) > 0:
-    for countour in countours:
-        if cv2.contourArea(countour) > 20 and cv2.contourArea(countour) < 120:
-            x, y, w, h = cv2.boundingRect(countour)
-            cv2.circle(persImage, (int(x + w/2), int(y + h/2)), int(h/2), (255, 0, 0), 5)
+# if len(countours) > 0:
+#     for countour in countours:
+#         # if cv2.contourArea(countour) > 100:
+#         x, y, w, h = cv2.boundingRect(countour)
+#         cv2.rectangle(persImage, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# invRedMask = cv2.bitwise_not(redMask)
+# countours, hierachy = cv2.findContours(invRedMask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# if len(countours) > 0:
+#     for countour in countours:
+#         # if cv2.contourArea(countour) > 20 and cv2.contourArea(countour) < 120:
+#         x, y, w, h = cv2.boundingRect(countour)
+#         cv2.circle(persImage, (int(x + w/2), int(y + h/2)), int(h/2), (255, 0, 0), 5)
 
 
-finalIm = cv2.cvtColor(hsvImage, cv2.COLOR_HSV2BGR)
+# finalIm = cv2.cvtColor(hsvImage, cv2.COLOR_HSV2BGR)
 
 cv2.imshow("board", persImage)
 
+cv2.imwrite('processed.png', persImage)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
