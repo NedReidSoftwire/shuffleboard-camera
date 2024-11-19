82a24f6acb3e2f6987008ace156ac7c7f41472bd
Ned Reid <Ned.Reid@softwire.com>
2024-11-19 17:28:47 +0000

    Add scoring UI

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import BoardView from "./visualisation/BoardView.tsx";
 import { io } from "socket.io-client";
 import {useEffect, useState} from "react";
-import {Disc, ShortCircuitGameState, TeamColour} from "../../types/types.ts";
+import {Disc, ShortCircuitGameState, ShortCircuitState, TeamColour} from "../../types/types.ts";
 
 function App() {
   const socket = io({
@@ -9,6 +9,7 @@ function App() {
   });
 
   const [testDiscs, setTestDiscs] = useState([] as Disc[])
+  const [shortCircuit, setShortCircuit] = useState<ShortCircuitState>()
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
@@ -19,6 +20,7 @@ function App() {
       });
       socket.on("short-circuit", (gameState: ShortCircuitGameState) => {
           setTestDiscs(gameState.discs)
+          setShortCircuit(gameState.shortCircuit)
       })
   }, []);
 
@@ -28,15 +30,27 @@ function App() {
       <div className="font-bold text-4xl p-4 text-center">
         Shuffleboard Camera
       </div>
-      <BoardView discs={testDiscs} />
-        <div className="w-full grid grid-cols-12 h-32 bg-purple-500 border-t-8 border-amber-200">
-            <div className="col-span-3 bg-blue-600 h-full p-4">
+      <BoardView discs={testDiscs} shortCircuit={shortCircuit} />
+        {shortCircuit && (
+            <div className="w-full grid grid-cols-12 h-32 bg-purple-500 border-t-8 border-amber-200">
+                <div className="col-span-3 bg-blue-600 h-full p-4">
+                    <div className="text-lg font-semibold text-white">Blue Distance:</div>
+                    <div
+                        className="text-4xl font-semibold text-white">{shortCircuit?.blueDistance?.distance ?? "😢"}</div>
+                </div>
+                <div className="col-span-6 bg-purple-300 h-full text-center">
+                    <div className="text-lg font-semibold text-white">current winner:</div>
+                    <div className="text-5xl font-semibold text-white">{shortCircuit.winner ?? "No-one ⚖️"}</div>
+
+                </div>
+                <div className="col-span-3 bg-red-500 h-full p-4 text-right">
                 <div className="text-lg font-semibold text-white">Blue Distance:</div>
-                <div className="text-4xl font-semibold text-white">{}</div>
+                    <div
+                        className="text-4xl font-semibold text-white">{shortCircuit?.blueDistance?.distance ?? "😢"}</div>
+
+                </div>
             </div>
-            <div className="col-span-6 bg-purple-300 h-full"></div>
-            <div className="col-span-3 bg-red-500 h-full"></div>
-        </div>
+        )}
     </div>
   );
 }
