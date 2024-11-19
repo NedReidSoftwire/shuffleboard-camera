7a1f189b0fc901e73339b3bfe616c15d75d054a1
Matt Williams <Matthew.Williams@Softwire.com>
2024-11-19 21:03:39 +0000

    initial CV processing tidy

--- camera/shuffleboard/shuffleView.py
+++ camera/shuffleboard/shuffleView.py
@@ -6,6 +6,8 @@ from enum import Enum
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 
+DEBUG = True
+
 class DiscColour(Enum):
   RED = 'Red'
   BLUE = 'Blue'
@@ -17,23 +19,27 @@ class Disc:
   y: int
   colour: DiscColour
 
-surface_size = (400, 1200)
+surface_dimensions = (400, 1200)
 board_corners = np.array([[389, 677], [401, 286], [1556, 241], [1607, 627]], dtype=np.float32) # BL, TL, TR, BR
 
 def __get_transformed_image(img, corners):
-    new_corners = np.array([[0, 0], [surface_size[0], 0], surface_size, [0, surface_size[1]]], dtype=np.float32)
+    new_corners = np.array([[0, 0], [surface_dimensions[0], 0], surface_dimensions, [0, surface_dimensions[1]]], dtype=np.float32)
 
     T = cv2.getPerspectiveTransform(corners, new_corners)
-    pers_image = cv2.warpPerspective(img, T, surface_size)
+    transformed_img = cv2.warpPerspective(img, T, surface_dimensions)
 
-    return pers_image
+    return transformed_img
 
-def __get_discs(img, colour):
+def __get_discs_by_colour(img, colour):
+    assert len(img.shape) == 3 and img.shape[2] == 3, "Provided image must have 3 bands."
+
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
     if colour == DiscColour.RED:
-        detections = __get_red_objects(img)
+        detections = __get_red_objects(img_hsv)
         colour_rgb = (0, 0, 255)
     elif colour == DiscColour.BLUE:
-        detections = __get_blue_objects(img)
+        detections = __get_blue_objects(img_hsv)
         colour_rgb = (255, 0, 0)
     else:
         raise Exception("Unsupported disc colour.")
@@ -53,80 +59,84 @@ def __get_discs(img, colour):
         maxRadius=35
     )
 
-    if circles is not None:
+    if circles is None:
+        return []
+
+    if DEBUG:
         debug_img = img.copy()
-        circles = np.uint16(np.around(circles))
 
-        discs = []
-        for x, y, radius in circles[0, :]:
-            discs.append(Disc(x=x.item(), y=surface_size[1]-y.item(), colour=colour))
+    circles = np.uint16(np.around(circles))
 
+    discs = []
+    for x, y, radius in circles[0, :]:
+        # NOTE: Due to the camera's position, the y-axis is to the center of the surface,
+        # we want the y-coordinates to begin at the end of the table.
+        flipped_y = surface_dimensions[1] - y.item()
+
+        discs.append(Disc(x=x.item(), y=flipped_y, colour=colour))
+
+        if DEBUG:
             center = (x, y)
             cv2.circle(debug_img, center, radius, colour_rgb, 2)
             cv2.circle(debug_img, center, 8, colour_rgb, -1)
-        
-        # cv2.imwrite(f'debug_detections_{colour.value}.png', debug_img)
+    
+    if DEBUG:
+        cv2.imwrite(f'debug_detections_{colour.value}.png', debug_img)
 
-        return discs
-    else:
-        return []
+    return discs
 
-def __get_red_objects(img):
-    hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
+def __get_red_objects(img_hsv):
+    # Note that red is split across the 0-180 degree range so we need to check two ranges.
     # colour map: https://i.sstatic.net/gyuw4.png
-    redLower = np.array([160, 150, 20])
-    redUpper = np.array([180, 255, 255])
-    redLower2 = np.array([0, 150, 20])
-    redUpper2 = np.array([20, 255, 255])
+    red_lower_1 = np.array([160, 150, 20])
+    red_upper_1 = np.array([180, 255, 255])
+    red_mask_1 = cv2.inRange(img_hsv, red_lower_1, red_upper_1)
 
-    redMask1 = cv2.inRange(hsvImage, redLower, redUpper)
-    redMask2 = cv2.inRange(hsvImage, redLower2, redUpper2)
-    redMask = cv2.bitwise_or(redMask1, redMask2)
-    red_objects = cv2.bitwise_and(img, img, mask=redMask)
+    red_lower_2 = np.array([0, 150, 20])
+    red_upper_2 = np.array([20, 255, 255])
+    red_mask_2 = cv2.inRange(img_hsv, red_lower_2, red_upper_2)
+
+    red_mask_combined = cv2.bitwise_or(red_mask_1, red_mask_2)
+    red_objects = cv2.bitwise_and(img_hsv, img_hsv, mask=red_mask_combined)
 
     return __get_binary_thresholded_img(red_objects)
 
-def __get_blue_objects(img):
-    hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def __get_blue_objects(img_hsv):
+    blue_lower = np.array([100, 140, 5])
+    blue_upper = np.array([130, 255, 255])
+    blue_mask = cv2.inRange(img_hsv, blue_lower, blue_upper)
 
-    blueLower = np.array([100, 140, 5])
-    blueUpper = np.array([130, 255, 255])
-
-    blueMask = cv2.inRange(hsvImage, blueLower, blueUpper)
-    blue_objects = cv2.bitwise_and(img, img, mask=blueMask)
+    blue_objects = cv2.bitwise_and(img_hsv, img_hsv, mask=blue_mask)
 
     return __get_binary_thresholded_img(blue_objects)
 
 def __get_binary_thresholded_img(img_hsv):
-    greyscale = cv2.split(img_hsv)[2]
-    _, detections = cv2.threshold(greyscale, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    img_greyscale = cv2.split(img_hsv)[2]
+    _, img_thresholded = cv2.threshold(img_greyscale, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    return detections
+    return img_thresholded
 
 def get_disc_coordinates(img):
     try:
-        transformed_image = __get_transformed_image(img, board_corners)
-        # cv2.imwrite('transformed_image.png', transformed_image)
+        img_transformed = __get_transformed_image(img, board_corners)
 
-        red_discs = __get_discs(transformed_image, DiscColour.RED)
-        blue_discs = __get_discs(transformed_image, DiscColour.BLUE)
-
-        # print('Detections:', len(red_discs), 'red', len(blue_discs), 'blue')
+        red_discs = __get_discs_by_colour(img_transformed, DiscColour.RED)
+        blue_discs = __get_discs_by_colour(img_transformed, DiscColour.BLUE)
 
         all_discs = red_discs + blue_discs
-        # print('all_discs', all_discs)
 
-        if len(all_discs) == 0:
-            raise Exception("No discs detected")
+        if DEBUG:
+            cv2.imwrite('debug_transformed_image.png', img_transformed)
+            print('Disc detections:', len(red_discs), 'red', len(blue_discs), 'blue')
+            print('all_discs:', all_discs)
 
         return all_discs
     except Exception as e:
-        # print('Error', e)
+        print('Error', e)
         return []
 
 if __name__ == '__main__':
     img = cv2.imread('capture.png')
 
     coords = get_disc_coordinates(img)
-    # print('coords', coords)
+    print('coords:', coords)
